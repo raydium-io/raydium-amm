aee4f1c248c82acb1a739d4dcc862bece4ade1d0
rain <rain@raydium.io>
2024-11-18 20:34:33 +0800

    Fix: add solana security txt

--- program/src/lib.rs
+++ program/src/lib.rs
@@ -15,4 +15,18 @@ pub mod state;
 // Export current solana-sdk types for downstream users who may also be building with a different solana-sdk version
 pub use solana_program;
 
-// solana_program::declare_id!("");
+#[cfg(not(feature = "no-entrypoint"))]
+solana_security_txt::security_txt! {
+    name: "raydium-amm",
+    project_url: "https://raydium.io",
+    contacts: "link:https://immunefi.com/bounty/raydium",
+    policy: "https://immunefi.com/bounty/raydium",
+    source_code: "https://github.com/raydium-io/raydium-amm",
+    preferred_languages: "en",
+    auditors: "https://github.com/raydium-io/raydium-docs/blob/master/audit/MadSheild%20Q2%202023/Raydium%20updated%20orderbook%20AMM%20program%20%26%20OpenBook%20migration.pdf"
+}
+
+#[cfg(feature = "devnet")]
+solana_program::declare_id!("HWy1jotHpo6UqeQxx49dpYYdQB8wj9Qk9MdxwjLvDHB8");
+#[cfg(not(feature = "devnet"))]
+solana_program::declare_id!("675kPX9MHTjS2zt1qfr1NYHuzeLXfQM9H24wFSUt1Mp8");
